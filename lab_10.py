de9c24ef9da3d380abc00092e46e61dbc9f3c97c
Philip Nedev <philipnedev@abv.bg>
2017-03-29 21:54:20 +0300

    Update Lab 10

--- lab_10.py
+++ lab_10.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 
-#create a function to display menu
+#create a function to display menu (you can use lab 9)
 #1 option sum
 #2 difference
 #q quit
+
+#Ask user for the numbers
+
+#create a function to check input( try/except )
 
 #create a function to calculate sum the two number
 
 #create a function to calculate the difference
 
-#create a function to check user imput
-
 #create a funciton to display the result
 
+
+
+#update your switch script
 
 
 
