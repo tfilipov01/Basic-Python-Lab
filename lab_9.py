45a746085d5c9224fc96a3b3736dbb12b3b7c9bc
Philip Nedev <philipnedev@abv.bg>
2017-03-29 21:51:34 +0300

    update lab 9

--- lab_9.py
+++ lab_9.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-#ask user for a number
+#Task 1
+# ask user for a number
 #convert it and use try except to handle wrong input
 '''
 try:
@@ -9,6 +10,12 @@ except:
     what to happend if the action is not successful and return error
 
 '''
+
+#Taks 2
+#Using While create menu
+#Using try/except convert the choice to integer
+#if input is q exit menu
+#if input not a number print error message
 
 
 
